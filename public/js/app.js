8b38f0425ba8cabb159f04c83855ce19d3fccd1f
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2014-09-15 15:00:09 +0100

    Registered the app controllers as modules

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', ['myApp.filters', 'myApp.directives']);
+var app = angular.module('myApp', [
+    'myApp.controllers',
+    'myApp.filters',
+    'myApp.directives'
+]);
